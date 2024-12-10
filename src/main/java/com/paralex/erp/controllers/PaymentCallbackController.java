032da4be5664d529f20911371b1815403a015587
nxg-hub <nextgenhubdigital@gmail.com>
2024-12-10 09:39:27 +0100

    callback url.

--- src/main/java/com/paralex/erp/controllers/PaymentCallbackController.java
+++ src/main/java/com/paralex/erp/controllers/PaymentCallbackController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller("/payment")
+@Controller
 public class PaymentCallbackController {
 
     private static final Logger logger = LoggerFactory.getLogger(PaymentCallbackController.class);
--- src/main/java/com/paralex/erp/interceptors/UserSessionTokenInterceptor.java
+++ src/main/java/com/paralex/erp/interceptors/UserSessionTokenInterceptor.java
@@ -34,7 +34,7 @@ public class UserSessionTokenInterceptor implements HandlerInterceptor {
             "POST /api/v1/auth/register",
             "POST /bail-bond/",
             "GET /bail-bond/",
-            "GET /payment/verify-transaction",
+            "GET /verify-transaction",
             "POST /service-provider/lawyer/profile/",
             "POST /service-provider/lawyer/profile/my",
             "GET /service-provider/lawyer/profile/",
