643e7900a9e29bd7b2fce7fa80b297d69cd2fe5c
nxg-hub <nextgenhubdigital@gmail.com>
2025-01-13 07:46:39 +0100

    location dto fix.

--- src/main/java/com/paralex/erp/dtos/CreateDriverProfileDto.java
+++ src/main/java/com/paralex/erp/dtos/CreateDriverProfileDto.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -79,9 +80,9 @@ public class CreateDriverProfileDto extends CreateProfileForUserDto {
     @NotBlank
     private String accountName;
 
-    @Schema(description = "Driver's location represented by a GeoJSON point",
-            example = "{\"type\": \"Point\", \"coordinates\": [0, 0]}")
-    private GeoJsonPoint location;
+    @Schema(description = "Driver's location represented by a GeoJSON point")
+    private Point location;
+
 
     @NotNull
     @NotEmpty
