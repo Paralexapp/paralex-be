da3fcd49b2c0cb5a59f7dedeed13a0c5a0a70f2d
Joseph Nma <josgolf@yahoo.ca>
2025-06-02 17:47:23 +0100

    adjust origin.

---
+++ src/main/java/com/paralex/erp/services/PingServiceImpl.java
@@ -0,0 +1,24 @@
+package com.paralex.erp.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class PingServiceImpl {
+
+    @Value("${server.url}")
+    private String serverUrl;  // Use your server URL here
+    @Scheduled(fixedRate = 250000)  // 5 minutes in milliseconds
+    public void pingServer() {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getForObject(serverUrl, String.class);
+            System.out.println("Server pinged at " + System.currentTimeMillis());
+        } catch (Exception e) {
+            System.err.println("Error while pinging the server: " + e.getMessage());
+        }
+    }
+}
